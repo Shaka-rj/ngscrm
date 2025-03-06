2a5f3e1446e9406e14df1bf58c088be28f3c3906
Shaka_Rj <shakarajabov@gmail.com>
2025-03-07 01:46:23 +0500

    2025-03-07 01:46:22

--- resources/views/user/spets_form2.blade.php
+++ resources/views/user/spets_form2.blade.php
@@ -1,7 +1,7 @@
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <link rel="stylesheet" href="{{ asset('css/spets.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/spets.css?v=1.0.1') }}">
 </head>
 <body>
     <div class="spets-container">
